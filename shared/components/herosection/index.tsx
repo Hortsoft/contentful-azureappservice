177c2e95e28363ff3c800fe9a96fb77d09bb39f7
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-07-14 14:36:55 +1200

    Simple Jumbotron

--- shared/components/herosection/index.tsx
+++ shared/components/herosection/index.tsx
@@ -1,29 +1,21 @@
-import React, { FunctionComponent } from 'react';
+import React from 'react';
 import Link from 'next/link';
  
 
   const HeroSection = () => (
-    <div className="container">
-    <Jumbotron className="text-white bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500">
-      <h1 className="text-2xl md:text-4xl mb-3">Jumbotron</h1>
-      <p className="mb-3">
-        What shallwe say. Gradient
-      </p>
-      
-      <button className="text-sm px-6 py-1 mt-4 text-white active:bg-yellow-500 focus:outline-none border-solid border">
+      <div>
+        <div className="jumbotron text-white bg-gradient-to-r from-blue-700 via-red-500 to-orange-500">
+            <h1 className="text-2xl md:text-4xl mb-3">Jumbotron</h1>
+        <p className="mb-3">
+            What shallwe say. Gradient
+        </p>
+        <button className="text-sm px-6 py-1 mt-4 text-white active:bg-yellow-500 focus:outline-none border-solid border">
         Learn more
-      </button>
-    </Jumbotron>
-    </div>
+        </button>
+        </div>
+        </div>
+    
   );
-  function Jumbotron({ children, className }) {
-    return (
-      <div
-        className={`${className} shadow-lg border font-light border-solid rounded-sm py-12 px-8 mb-8`}
-      >
-        {children}
-      </div>
-    );
-  }
+   
   
   export default HeroSection;

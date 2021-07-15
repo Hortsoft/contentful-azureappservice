5071d8ddc8fa6ba18580cb7bf19f680252ec79ee
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-07-15 17:37:19 +1200

    Branch 8 React Player

---
+++ pages/react_player.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactPlayer from 'react-player'
+
+export const TheReactPlayer = () => {
+  return <>
+   <div className="container mt-5 mx-auto px-2">
+  <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+  
+  </div>
+  </>;
+};
+ 

aca0d8b9b5f03f2189f93ebb70f6b943861ca242
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-08-25 14:07:07 +1200

    footer customization

--- shared/components/footer/index.tsx
+++ shared/components/footer/index.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
     return (
       <>
          
-<section className="bg-white">
+<section className="pb-10 mt-6 bg-gray-200">
     <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
         <nav className="flex flex-wrap justify-center -mx-5 -my-2">
             <div className="px-5 py-2">
